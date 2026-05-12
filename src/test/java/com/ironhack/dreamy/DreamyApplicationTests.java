ef76cb35242e04acedf8183d0d1250ced442f6bb
yagutrslbyl <yaguter-bpa202@code.edu.az>
2026-05-12 16:32:22 +0400

    first commit

---
+++ src/test/java/com/ironhack/dreamy/DreamyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.dreamy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
