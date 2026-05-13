9007ff026b86a22e9b7fc77968e73d799d911665
Shebnem.M <snmmrdv@gmail.com>
2026-05-13 14:38:57 +0400

    update bookrequest, bookresponse, errorresponse

--- src/main/java/com/ironhack/dreamy/dto/response/BookResponse.java
+++ src/main/java/com/ironhack/dreamy/dto/response/BookResponse.java
@@ -1,8 +1,15 @@
 package com.ironhack.dreamy.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.internal.build.AllowNonPortable;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookResponse {
     private Long id;
     private String title;
--- src/main/java/com/ironhack/dreamy/dto/response/ErrorResponse.java
+++ src/main/java/com/ironhack/dreamy/dto/response/ErrorResponse.java
@@ -1,10 +1,15 @@
 package com.ironhack.dreamy.dto.response;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class ErrorResponse {
         private LocalDateTime timestamp;
         private int status;
