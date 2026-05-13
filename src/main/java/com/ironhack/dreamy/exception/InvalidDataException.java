bf2873e5a8cf5fc8572af1f0b0be756f5e6dd783
Shebnem.M <snmmrdv@gmail.com>
2026-05-13 16:51:49 +0400

    feat: add exceptions

---
+++ src/main/java/com/ironhack/dreamy/exception/InvalidDataException.java
@@ -0,0 +1,8 @@
+package com.ironhack.dreamy.exception;
+
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String message)
+    {
+        super(message);
+    }
+}
