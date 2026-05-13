70b534fe9648321f9d0761d15aad4f4f465c794b
gurban200OK <qurbankerem@gmail.com>
2026-05-14 03:21:25 +0400

    feat: update DreamyApplicationTests class

--- src/test/java/com/ironhack/dreamy/DreamyApplicationTests.java
+++ src/test/java/com/ironhack/dreamy/DreamyApplicationTests.java
@@ -2,8 +2,12 @@ package com.ironhack.dreamy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class DreamyApplicationTests {
 
     @Test
